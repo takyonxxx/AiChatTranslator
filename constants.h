13e319f6ac7164ca355b6ad726e58859cb8560d3
TÜRKAY BİLİYOR <turkaybiliyor@hotmail.com>
2025-10-11 17:09:11 +0300

    fix

--- constants.h
+++ constants.h
@@ -1,11 +1,1 @@
-#ifndef CONSTANTS_H
-#define CONSTANTS_H
-#include <QString>
-static QString speechBaseApi = "https://speech.googleapis.com/v1/speech:recognize";
-static QString speechApiKey = "AIzaSyCY8Xg5wfn6Ld67287SGDBQPZvGCEN6Fsg";
-static QString translateUrl = "https://translated-mymemory---translation-memory.p.rapidapi.com/get";
-static QString translateHost = "translated-mymemory---translation-memory.p.rapidapi.com";
-static QString translateApiKey = "413237f255msh65570d4eb42a8a7p1d9144jsnea8334feeacc";
-const QString googleTtsBaseApi = "https://texttospeech.googleapis.com/v1/text:synthesize";
-const QString claudeApiKey = "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";
-#endif // CONSTANTS_H
+
