0643944e916e012a6d76856034ebc3f7b0d34a8d
TÜRKAY BİLİYOR <turkaybiliyor@hotmail.com>
2025-10-11 12:06:40 +0300

    fix

--- constants.h
+++ constants.h
@@ -1,5 +1,12 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 #include <QString>
-
+static QString speechBaseApi = "https://speech.googleapis.com/v1/speech:recognize";
+static QString speechApiKey = "silAIzaSyCY8Xg5wfn6Ld67287SGDBQPZvGCEN6Fsgsil";
+static QString translateUrl = "https://translated-mymemory---translation-memory.p.rapidapi.com/get";
+static QString translateHost = "translated-mymemory---translation-memory.p.rapidapi.com";
+static QString translateApiKey = "sil413237f255msh65570d4eb42a8a7p1d9144jsnea8334feeaccsil";
+static QString aiUrl = "";
+static QString aiHost = "";
+static QString aiApiKey = "";
 #endif // CONSTANTS_H
