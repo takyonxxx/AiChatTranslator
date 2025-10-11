4eaca87ca742f97a98b76e81a30a136919e63f9c
TÜRKAY BİLİYOR <turkaybiliyor@hotmail.com>
2025-10-11 12:04:53 +0300

    fix

--- constants.h
+++ constants.h
@@ -1,12 +1,5 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 #include <QString>
-static QString speechBaseApi = "https://speech.googleapis.com/v1/speech:recognize";
-static QString speechApiKey = "AIzaSyCY8Xg5wfn6Ld67287SGDBQPZvGCEN6Fsg";
-static QString translateUrl = "https://translated-mymemory---translation-memory.p.rapidapi.com/get";
-static QString translateHost = "translated-mymemory---translation-memory.p.rapidapi.com";
-static QString translateApiKey = "413237f255msh65570d4eb42a8a7p1d9144jsnea8334feeacc";
-static QString aiUrl = "";
-static QString aiHost = "";
-static QString aiApiKey = "";
+
 #endif // CONSTANTS_H
